b7a95629168eb0e1dbb01d650dae9c5c18c9c120
Brett <bonifas.fetzer@gmail.com>
2020-03-19 22:11:23 -0400

    Update genCommand

--- src/main/java/hackandslash/hackandslash/generateCommand.java
+++ src/main/java/hackandslash/hackandslash/generateCommand.java
@@ -1,47 +1,57 @@
 package hackandslash.hackandslash;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class generateCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 3) {
-            switch (args[1].toUpperCase()) {
-                case "STRENGTH":{
-                    generateType1Dungeon(args[0], args[3]);
-                    break;
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 3) {
+                try {
+                    switch (args[1].toUpperCase()) {
+                        case "STRENGTH":{
+                            generateType1Dungeon(args[0], Integer.parseInt(args[3]), player.getLocation());
+                            break;
+                        }
+                        case "INTELECT":{
+                            generateType2Dungeon(args[0], Integer.parseInt(args[3]), player.getLocation());
+                            break;
+                        }
+                        case "WALKTHROUGH":{
+                            generateType3Dungeon(args[0], Integer.parseInt(args[3]), player.getLocation());
+                            break;
+                        }
+                        default: {
+                            sender.sendMessage("Invalid Dungeon Type!");
+                        }
+        
+                    }
+                } catch (NumberFormatException exception) {
+                    sender.sendMessage("Size must be a number!");
                 }
-                case "INTELECT":{
-                    generateType2Dungeon(args[0], args[3]);
-                    break;
-                }
-                case "WALKTHROUGH":{
-                    generateType3Dungeon(args[0], args[3]);
-                    break;
-                }
-                default: {
-                    sender.sendMessage("Invalid Dungeon Type!");
-                }
-
+                
+            } else {
+                sender.sendMessage("Usage: /generatedungeon <name> <type> <sizeInChunks>");
             }
-        } else {
-            sender.sendMessage("Usage: /generatedungeon <name> <type> <sizeInChunks>");
         }
         return false;
     }
 
-    public void generateType1Dungeon(String name, int size) {
+    public void generateType1Dungeon(String name, int size, Location loc) {
 
     }
 
-    public void generateType2Dungeon(String name, int size) {
+    public void generateType2Dungeon(String name, int size, Location loc) {
 
     }
 
-    public void generateType3Dungeon(String name, int size) {
+    public void generateType3Dungeon(String name, int size, Location loc) {
 
     }
 
