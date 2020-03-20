99e889986388d247d1a62d3b4ad53dc2e1e86492
ShiftySnowWolf <jj42001@gmail.com>
2020-03-19 20:58:51 -0500

    Basic Changes

--- src/main/java/hackandslash/hackandslash/HackAndSlash.java
+++ src/main/java/hackandslash/hackandslash/HackAndSlash.java
@@ -4,14 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HackAndSlash extends JavaPlugin {
 
+    public static HackAndSlash plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        plugin = this;
+        
+        System.out.println("Hack and slash loading!");
+        // /generatedungeon name type sizeInChunks
+        this.getCommand("generatedungeon").setExecutor(new generateCommand());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
     }
 }
