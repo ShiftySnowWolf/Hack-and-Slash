caf73d4da98102f31d1d646e417e1d539f48800b
Brett <bonifas.fetzer@gmail.com>
2020-03-19 22:20:38 -0400

    Command Sender Separation

--- src/main/java/hackandslash/hackandslash/generateCommand.java
+++ src/main/java/hackandslash/hackandslash/generateCommand.java
@@ -1,6 +1,7 @@
 package hackandslash.hackandslash;
 
 import org.bukkit.Location;
+import org.bukkit.block.CommandBlock;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,20 +12,42 @@ public class generateCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
-            Player player = (Player) sender;
+            ifPlayer(sender, args);
+        } else if (sender instanceof CommandBlock) {
+            ifCommBlock(sender, args);
+        } else {
+            sender.sendMessage("This command cannot be run from console!");
+        }
+        return false;
+    }
+
+    public void generateType1Dungeon(String name, int size, Location loc) {
+        
+    }
+
+    public void generateType2Dungeon(String name, int size, Location loc) {
+
+    }
+
+    public void generateType3Dungeon(String name, int size, Location loc) {
+
+    }
+
+    public void ifPlayer(CommandSender sender, String[] args) {
+        Player player = (Player) sender;
             if (args.length == 3) {
                 try {
                     switch (args[1].toUpperCase()) {
                         case "STRENGTH":{
-                            generateType1Dungeon(args[0], Integer.parseInt(args[3]), player.getLocation());
+                            generateType1Dungeon(args[0], Integer.parseInt(args[2]), player.getLocation());
                             break;
                         }
                         case "INTELECT":{
-                            generateType2Dungeon(args[0], Integer.parseInt(args[3]), player.getLocation());
+                            generateType2Dungeon(args[0], Integer.parseInt(args[2]), player.getLocation());
                             break;
                         }
                         case "WALKTHROUGH":{
-                            generateType3Dungeon(args[0], Integer.parseInt(args[3]), player.getLocation());
+                            generateType3Dungeon(args[0], Integer.parseInt(args[2]), player.getLocation());
                             break;
                         }
                         default: {
@@ -39,28 +62,37 @@ public class generateCommand implements CommandExecutor {
             } else {
                 sender.sendMessage("Usage: /generatedungeon <name> <type> <sizeInChunks>");
             }
-        }
-        return false;
     }
 
-    public void generateType1Dungeon(String name, int size, Location loc) {
-
-    }
-
-    public void generateType2Dungeon(String name, int size, Location loc) {
-
-    }
-
-    public void generateType3Dungeon(String name, int size, Location loc) {
-
-    }
-
-    public void ifPlayer() {
-
-    }
-
-    public void ifCommBlock() {
-
+    public void ifCommBlock(CommandSender sender, String[] args) {
+        CommandBlock commBlock = (CommandBlock) sender;
+            if (args.length == 3) {
+                try {
+                    switch (args[1].toUpperCase()) {
+                        case "STRENGTH":{
+                            generateType1Dungeon(args[0], Integer.parseInt(args[2]), commBlock.getLocation());
+                            break;
+                        }
+                        case "INTELECT":{
+                            generateType2Dungeon(args[0], Integer.parseInt(args[2]), commBlock.getLocation());
+                            break;
+                        }
+                        case "WALKTHROUGH":{
+                            generateType3Dungeon(args[0], Integer.parseInt(args[2]), commBlock.getLocation());
+                            break;
+                        }
+                        default: {
+                            sender.sendMessage("Invalid Dungeon Type!");
+                        }
+        
+                    }
+                } catch (NumberFormatException exception) {
+                    sender.sendMessage("Size must be a number!");
+                }
+                
+            } else {
+                sender.sendMessage("Usage: /generatedungeon <name> <type> <sizeInChunks>");
+            }
     }
 
 }
