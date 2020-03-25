6f382f2b698789ead7f47fb761a605bfe485f1ea
bonn2 <bonifas.fetzer@gmail.com>
2020-03-24 23:47:04 -0400

    Created PasteLocation Math Class

---
+++ src/main/java/bonnett/data/math/PasteLocation.java
@@ -0,0 +1,36 @@
+package bonnett.data.math;
+
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.math.BlockVector3;
+import org.bukkit.Location;
+
+public class PasteLocation {
+    Location pasteLocation;
+
+    public PasteLocation(Location destination, Clipboard clipboard) {
+        pasteLocation = findPasteLocation(destination, clipboard);
+    }
+
+    public Location getPasteLocation() {
+        return pasteLocation;
+    }
+
+    private Location findPasteLocation(Location destination, Clipboard clipboard) {
+        BlockVector3 copyLoc = clipboard.getOrigin();
+        BlockVector3 cornerMin = clipboard.getRegion().getMinimumPoint();
+        BlockVector3 offset = BlockVector3.at(
+                cornerMin.getX() - copyLoc.getX(),
+                cornerMin.getY() - copyLoc.getY(),
+                cornerMin.getZ() - copyLoc.getZ());
+        Location pasteLocation = new Location(destination.getWorld(),
+                destination.getX() - offset.getX(),
+                destination.getY() - offset.getY(),
+                destination.getZ() - offset.getZ());
+        return pasteLocation;
+    }
+
+    private BlockVector3 locationToBlockVector3(Location location) {
+        return BlockVector3.at(location.getX(), location.getY(), location.getZ());
+    }
+
+}
