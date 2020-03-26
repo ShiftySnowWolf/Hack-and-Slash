de388a0d0f3b93af00ee220d527fa42ef58bf4b3
Jonathan <jj42001@gmail.com>
2020-03-26 11:59:18 -0500

    Implemented LuckPerms

--- src/main/java/bonnett/data/PluginTabCompleter.java
+++ src/main/java/bonnett/data/PluginTabCompleter.java
@@ -14,18 +14,27 @@ public class PluginTabCompleter implements TabCompleter {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> output;
         String[] validTypes = Main.validPalettes;
-        if (args.length == 1) {
-            if (validTypes == null) {
+
+        switch (args.length) {
+            case 1: {
                 output = new ArrayList<>(0);
+                output.add("generate");
+                output.add("reloadpalettes");
                 return output;
             }
-            output = new ArrayList<>(validTypes.length);
-            for (String string : validTypes) {
-                if (string.startsWith(args[0])) {
-                    output.add(string);
+            case 2: {
+                if (validTypes == null) {
+                    output = new ArrayList<>(0);
+                    return output;
                 }
+                output = new ArrayList<>(validTypes.length);
+                for (String s : validTypes) {
+                    if (s.startsWith(args[0])) {
+                        output.add(s);
+                    }
+                }
+                return output;
             }
-            return output;
         }
         output = new ArrayList<>(0);
         return output;
