4cb747ffc9b9e784a21ff61a7271835c3acbcc1d
Jonathan <jj42001@gmail.com>
2020-03-26 22:24:23 -0500

    TabCompleter implemented!

--- src/main/java/bonnett/commands/PluginTabCompleter.java
+++ src/main/java/bonnett/commands/PluginTabCompleter.java
@@ -4,41 +4,44 @@ import bonnett.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PluginTabCompleter implements TabCompleter {
+
+    Main plugin = Main.plugin;
     
     @Override
-    public List<String> onTabComplete(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String alias, String[] args) {
+    public List<String> onTabComplete(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String alias, @ Nonnull String[] args) {
         if (!sender.hasPermission("chunkdungeons.admin")) { return null; }
-        List<String> output;
+        List<String> commands = new ArrayList<>();
+        List<String> completion = new ArrayList<>();
+        boolean isGenerate = args[0].equalsIgnoreCase("generate");
+
         String[] validTypes = Main.validPalettes;
+
+        int min = Main.min_size;
+        int max = Main.max_size;
+        List<String> range = new ArrayList<>(0);
+        for (int count = min; count <= max; count++) { range.add(String.valueOf(count)); }
 
         switch (args.length) {
             case 1: {
-                output = new ArrayList<>(0);
-                output.add("generate");
-                output.add("reloadpalettes");
-                return output;
-            }
-            case 2: {
-                if (validTypes == null) {
-                    output = new ArrayList<>(0);
-                    return output;
-                }
-                output = new ArrayList<>(validTypes.length);
-                for (String s : validTypes) {
-                    if (s.startsWith(args[0])) {
-                        output.add(s);
-                    }
-                }
-                return output;
-            }
+                commands.add("generate");
+                commands.add("reload");
+                commands.add("reloadconfig");
+                commands.add("reloadpalettes");
+                StringUtil.copyPartialMatches(args[0], commands, completion);
+            } break;
+            case 2: { if (isGenerate) { commands.addAll(Arrays.asList(validTypes)); }} break;
+            case 3: { if (isGenerate && Arrays.asList(validTypes).contains(args[1])) { commands.addAll(range); }} break;
         }
-        output = new ArrayList<>(0);
-        return output;
+        Collections.sort(completion);
+        return completion;
     }
 }
